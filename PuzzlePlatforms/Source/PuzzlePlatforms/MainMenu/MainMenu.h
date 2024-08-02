b4d658af7c8c644d6be686f0df90a5adfb622e36
Dan2908 <63173102+Dan2908@users.noreply.github.com>
2024-08-02 16:01:24 -0300

    Lecture 35 (10/25)
    Initialisers and UButton Callbacks.

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/MainMenu/MainMenu.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "Blueprint/UserWidget.h"
+#include "MainMenu.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PUZZLEPLATFORMS_API UMainMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+
+	virtual bool Initialize() override;
+
+	UFUNCTION()
+	void HostServer();
+
+private:
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BTN_Host;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* BTN_Join;
+	
+};
